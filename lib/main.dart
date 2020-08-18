5a37f09d9219765cdaf95a9deff3bdc116c1ee79
Baksman <Ibrahimshehu73700@gmail.com>
2020-08-18 11:47:48 +0100

    added animated text kit and firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
-
+import 'package:google_fonts/google_fonts.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
@@ -12,15 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Ibrahims portforlio',
+      title: 'Baksman',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-
-        primarySwatch: Colors.blue,
-
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
+      theme: ThemeData.dark(),
       home: Home(),
     );
   }
@@ -35,70 +31,293 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  color: Colors.red,
-                  child: Row(
-                    children: [
-                      Spacer(),
-                      Icon(
-                        Icons.menu,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 20,
-                      )
-                    ],
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            // Row(
+            //   children: [
+            //     Expanded(
+            //       child: Container(
+            //         // height: 50,
+            //         width: double.infinity,
+            //         color: Colors.red,
+            //         child: Row(
+            //           children: [
+            //             Spacer(),
+            //             Icon(
+            //               Icons.menu,
+            //               color: Colors.white,
+            //             ),
+            //             SizedBox(
+            //               width: 20,
+            //             )
+            //           ],
+            //         ),
+            //       ),
+            //     )
+            //   ],
+            // ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    launchUrl("https:www.google.com");
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    height: 60,
+                    child: Text(
+                      "PORTFOLIO",
+                      style: TextStyle(
+                          fontSize: 20, fontFamily: "AnnieUseYourTelescope"),
+                    ),
                   ),
                 ),
-              )
-            ],
-          ),
-          Expanded(
-              child: ListView(
-            children: [
-              Text(
-                "I am Ibrahim Ibrahim Shehu",
-                style: TextStyle(fontWeight: FontWeight.w600, fontSize: 35),
-              ),
-              Row(
-                children: [
-                  Text("I do",style: textStyle(25),),
-                  SizedBox(
-                    width: 10,
+                GestureDetector(
+                  onTap: () {
+                    launchUrl("https://twitter.com/ibrahimshehuib4");
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    height: 60,
+                    child: Text(
+                      "CANTACT ME",
+                      style: TextStyle(
+                          fontSize: 20, fontFamily: "AnnieUseYourTelescope"),
+                    ),
                   ),
-                  RotateAnimatedTextKit(
-                      repeatForever: true,
-                      duration: Duration(seconds: 1),
-                      textStyle:textStyle(25),
-                      text: ["Flutter", "Go", "Django"]),
-                  SizedBox(
-                    width: 5,
+                ),
+                GestureDetector(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    height: 60,
+                    child: Text(
+                      "RESUME",
+                      style: TextStyle(
+                          fontSize: 20, fontFamily: "AnnieUseYourTelescope"),
+                    ),
                   ),
-                  // Text("Developer")
-                ],
-              ),
-              Text(
-                "A passion driven mobile & backend developer",
-                style: TextStyle(fontSize: 25),
-              ),
-            ],
-          ))
-        ],
+                ),
+                GestureDetector(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    height: 60,
+                    child: Text(
+                      "HIRE",
+                      style: TextStyle(
+                          fontSize: 20, fontFamily: "AnnieUseYourTelescope"),
+                    ),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: ListView(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Hi, I am Engr.",
+                      style: textStyle(25),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    TypewriterAnimatedTextKit(
+                      speed: Duration(milliseconds: 100),
+                      text: [
+                        "Ibrahim Ibrahim Shehu",
+                      ],
+                      textStyle: textStyle(25),
+                    )
+                  ],
+                ),
+
+                // Text(
+                //   "A passion driven mobile & backend developer",
+                //   style: TextStyle(fontSize: 25),
+                // ),
+                // TypewriterAnimatedTextKit(
+                //   speed: Duration(milliseconds: 100),
+                //   text: [
+                //     "A passion driven mobile & backend developer",
+                //     "Creator of Get Job mobile application"
+                //   ],
+                //   textStyle: textStyle(25),
+                // ),
+                SizedBox(height: 20),
+                Center(
+                  child: Container(
+                    height: 200,
+                    width: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        // boxShadow: [],
+                        color: Colors.teal,
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("assets/my_pic.jpg"))),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Center(
+                    child: Text(
+                  "A flutter mobile developer",
+                  style: textStyle(25),
+                )),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "I create beautiful",
+                      style: TextStyle(
+                          fontSize: 20, fontFamily: "AnnieUseYourTelescope"),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    RotateAnimatedTextKit(
+                        repeatForever: true,
+                        duration: Duration(seconds: 2),
+                        textStyle: TextStyle(
+                            fontSize: 20, fontFamily: "AnnieUseYourTelescope"),
+                        text: ["mobile", "web", "desktop"]),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Text(
+                      "apps with Flutter",
+                      style: TextStyle(
+                          fontSize: 20, fontFamily: "AnnieUseYourTelescope"),
+                    )
+                  ],
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "I'm also skilled in ",
+                      style: TextStyle(fontSize: 20, fontFamily: "DoHyeon"),
+                    ),
+                    TypewriterAnimatedTextKit(
+                        speed: Duration(seconds: 1),
+                        repeatForever: true,
+                        text: [
+                          "Golang",
+                          "Python",
+                          "Django",
+                          // "Do not test bugs out, design them out",
+                        ],
+                        textStyle: TextStyle(
+                            fontSize: 20, fontFamily: "AnnieUseYourTelescope"),
+                        textAlign: TextAlign.start,
+                        alignment: AlignmentDirectional
+                            .topStart // or Alignment.topLeft
+                        ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                // iconContainer()
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        launchUrl("https:github.com/baksman");
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        child: Center(child: FaIcon(FontAwesomeIcons.github)),
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        launchUrl(
+                            "https://play.google.com/store/apps/details?id=com.baksman.hiring");
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        child: Center(child: FaIcon(FontAwesomeIcons.appStore)),
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        launchUrl("https://twitter.com/ibrahimshehuib4");
+                        // https://twitter.com/ibrahimshehuib4
+                      },
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        child: Center(child: FaIcon(FontAwesomeIcons.twitter)),
+                        decoration: BoxDecoration(
+                            color: Colors.black,
+                            borderRadius: BorderRadius.circular(20)),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ))
+          ],
+        ),
       ),
     );
   }
 
   TextStyle textStyle(double fontSize) {
-    return TextStyle(
-      fontSize: fontSize,
-      // color:
-    );
+    return TextStyle(fontSize: fontSize, fontFamily: "DoHyeon"
+        // color:
+        );
+  }
+
+  // Container iconContainer(FontAwesomeIcons icon) {
+  //   return Container(
+  //     child: FaIcon(FontAwesomeIcons.gamepad),
+  //   );
+  // }
+
+  Future<void> launchUrl(String url) async {
+    if (await canLaunch(url)) {
+      print("i can");
+      await launch(
+        url,
+        forceWebView: false,
+      );
+    } else {
+      print("cant launc url");
+    }
   }
 }
